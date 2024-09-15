cbbede85f564cbffeea99ad72d0bfb7b18aaff18
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-09-15 09:24:31 +0300

    add: flake.nix

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Nix Flake for SpiecEasi R package";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        SpiecEasi = pkgs.rPackages.buildRPackage {
+          name = "SpiecEasi";
+          src = self;
+          propagatedBuifldInputs = with pkgs.rPackages; [
+            MASS
+            Matrix
+            RcppArmadillo
+            VGAM
+            glmnet
+            huge
+            pulsar
+          ];
+        };
+      in
+      {
+        packages.default = SpiecEasi;
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ SpiecEasi ];
+        };
+      }
+    );
+}
